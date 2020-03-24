f7303975d764325f13d89ed34c2c1d439c458bdb
emi <emiliorizzo@gmail.com>
2020-03-24 23:06:34 +0100

    add data.sh

---
+++ data.sh
@@ -0,0 +1,19 @@
+# !/usr/bin/bash
+
+echo 'Sep'
+node txStats.js 1672387 1753420
+
+echo 'Oct'
+node txStats.js 1753421 1835701
+
+echo 'Nov'
+node txStats.js 1835702 1917127
+
+echo 'Dec'
+node txStats.js 1917128 1999071
+
+echo 'Jan'
+node txStats.js 1999072 2079443
+
+echo 'Feb'
+node txStats.js 2079444 2155040
